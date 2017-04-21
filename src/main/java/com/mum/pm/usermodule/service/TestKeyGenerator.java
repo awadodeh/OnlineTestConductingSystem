c7fefacf79011eb0da8fc63142800161feafdff8
Chuang Huang <chuang.huang12@gmail.com>
2017-04-21 09:44:13 -0500

    Key Generator

---
+++ src/main/java/com/mum/pm/usermodule/service/TestKeyGenerator.java
@@ -0,0 +1,8 @@
+package com.mum.pm.usermodule.service;
+
+/**
+ * Created by Chuang on 2017/4/20.
+ */
+public interface TestKeyGenerator {
+    boolean generateTestKey(String studentId, String email, String coachId);
+}
---
+++ src/main/java/com/mum/pm/usermodule/service/TestKeyGeneratorImpl.java
@@ -0,0 +1,11 @@
+package com.mum.pm.usermodule.service;
+
+/**
+ * Created by Chuang on 2017/4/20.
+ */
+public class TestKeyGeneratorImpl implements TestKeyGenerator {
+    @Override
+    public boolean generateTestKey(String studentId, String email, String coachId) {
+        return false;
+    }
+}
